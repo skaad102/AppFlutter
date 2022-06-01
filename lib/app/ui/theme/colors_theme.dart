2b4312070c35c3d42814b04ff8a8b8f31082e522
daniel adame <skaad102@gmail.com>
2022-06-01 09:08:25 -0500

    crear thema personalizado

--- lib/app/ui/theme/colors_theme.dart
+++ lib/app/ui/theme/colors_theme.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ColorTheme {
   static Color primary = Color(0xFF00b894);
@@ -6,4 +6,21 @@ class ColorTheme {
   static Color tercery = Color(0xFF0984e3);
   static Color background = Color(0xFF6c5ce7);
   static Color text = Color(0xFF636e72);
+
+  static ThemeData get materialTheme {
+    return ThemeData(
+      primaryColor: primary,
+      textTheme: TextTheme(
+        bodyText1: TextStyle(color: text),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+          padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+            const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          ),
+          backgroundColor: MaterialStateProperty.all<Color>(primary),
+        ),
+      ),
+    );
+  }
 }
