45a4544c93412c2143dcbf67a5aa570a8e415e86
daniel adame <skaad102@gmail.com>
2022-06-01 19:21:10 -0500

    pagina para abrir configuracion del gps

---
+++ lib/app/ui/pages/acceso_page.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import '../theme/colors_theme.dart';
+import 'package:rutas_app/app/utils/trancicion_utils.dart';
+import '../pages/loading_page.dart';
+import '../widget/buton_widget.dart';
+
+import 'mapa_page.dart';
+
+class AccesoGpsPage extends StatefulWidget {
+  @override
+  State<AccesoGpsPage> createState() => _AccesoGpsPageState();
+}
+
+class _AccesoGpsPageState extends State<AccesoGpsPage>
+    with WidgetsBindingObserver {
+  @override
+  void initState() {
+    WidgetsBinding.instance?.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance?.removeObserver(this);
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) async {
+    print(state);
+    /* nuevamente entra al app */
+    if (state == AppLifecycleState.resumed) {
+      /* validar si tenmos acceso */
+      if (await Permission.location.isGranted) {
+        transicionPage(context, LoadingPage());
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "¡nececitamos acceso a tu hubicación!".toUpperCase(),
+              style: TextStyle(color: ColorTheme.text),
+            ),
+            ButonWidget(
+              text: 'Acceder',
+              onPressed: onPressAcces,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onPressAcces() async {
+    final status = await Permission.location.request();
+    print(status);
+    switch (status) {
+      case PermissionStatus.granted:
+        // openAppSettings();
+        transicionPage(context, MapaPage());
+        break;
+      case PermissionStatus.restricted:
+      case PermissionStatus.permanentlyDenied:
+      case PermissionStatus.denied:
+        openAppSettings();
+        break;
+      case PermissionStatus.limited:
+        // TODO: Handle this case.
+        break;
+    }
+  }
+}
