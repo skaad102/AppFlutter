5030e41ded27fe48ffed5ea8b471c31bd2663fd7
daniel adame <skaad102@gmail.com>
2022-06-01 19:17:45 -0500

    config tema

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:rutas_app/app/ui/routes/rutas.dart';
+import 'package:rutas_app/app/ui/theme/colors_theme.dart';
+import './app/ui/pages/loading_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,17 +9,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      home: LoadingPage(),
+      initialRoute: 'LoadingPage',
+      routes: routes,
+      theme: ColorTheme.materialTheme,
     );
   }
 }
