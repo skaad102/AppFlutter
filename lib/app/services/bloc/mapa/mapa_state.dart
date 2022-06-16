428e4cc79474dd22865040b7404c22afc9f02149
daniel adame <skaad102@gmail.com>
2022-06-15 19:07:27 -0500

    estado iniiales

--- lib/app/services/bloc/mapa/mapa_state.dart
+++ lib/app/services/bloc/mapa/mapa_state.dart
@@ -2,10 +2,34 @@ part of 'mapa_bloc.dart';
 
 class MapaStateInitial {
   final bool mapaListo;
+  final bool dibujarRuta;
+  final bool seguirRuta;
+  final LatLng ubicacionCentral;
+  final Map<String, Polyline>? polylines;
 
-  MapaStateInitial({this.mapaListo = false});
+  /* TODO : Refact */
+  MapaStateInitial(
+      {this.mapaListo = false,
+      this.dibujarRuta = true,
+      this.seguirRuta = false,
+      this.ubicacionCentral = const LatLng(0, 0),
+      Map<String, Polyline>? polylines})
+      // this.polylines = const {}});
+      : polylines = polylines ?? {};
 
-  MapaStateInitial copyWith({bool? mapaListo}) {
-    return MapaStateInitial(mapaListo: mapaListo ?? this.mapaListo);
+/* emitir nuevos estados del mapa */
+  MapaStateInitial copyWith({
+    bool? mapaListo,
+    bool? dibujarRuta,
+    bool? seguirRuta,
+    LatLng? ubicacionCentral,
+    Map<String, Polyline>? polylines,
+  }) {
+    return MapaStateInitial(
+        mapaListo: mapaListo ?? this.mapaListo,
+        dibujarRuta: dibujarRuta ?? this.dibujarRuta,
+        seguirRuta: seguirRuta ?? this.seguirRuta,
+        ubicacionCentral: ubicacionCentral ?? this.ubicacionCentral,
+        polylines: polylines ?? this.polylines);
   }
 }
