1c531ebe7776b93a1a83bf0a9600640242565b58
daniel adame <skaad102@gmail.com>
2022-06-15 19:08:26 -0500

    bton segimieto

---
+++ lib/app/ui/widget/buton_seguir.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rutas_app/app/services/bloc/mapa/mapa_bloc.dart';
+
+class BtnFollow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final bloc = context.read<MapaBloc>();
+    return BlocBuilder<MapaBloc, MapaStateInitial>(
+      builder: (context, state) {
+        return Container(
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                CircleAvatar(
+                  child: FloatingActionButton(
+                    onPressed: () {
+                      bloc.add(SeguirRuta());
+                    },
+                    child: (bloc.state.seguirRuta)
+                        ? const Icon(Icons.accessibility_new)
+                        : const Icon(Icons.directions_walk_outlined),
+                  ),
+                )
+              ],
+            ));
+      },
+    );
+  }
+}
