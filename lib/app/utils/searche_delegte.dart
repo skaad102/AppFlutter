4134e3d26362108209cd13799ca545239bd88ae3
daniel adame <skaad102@gmail.com>
2022-08-08 18:45:38 -0500

    mustrar listado en seach

--- lib/app/utils/searche_delegte.dart
+++ lib/app/utils/searche_delegte.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../model/search_model.dart';
+import '../services/api/ruta_point_api.dart';
 import '../services/bloc/busqueda/busqueda_bloc.dart';
+import 'package:rutas_app/app/model/search_gecoding.dart';
+
+import '../services/bloc/mi_ubicacion/mi_ubicacion_bloc.dart';
 
 class MapSearchDelegate extends SearchDelegate<SearchResult> {
+  final RutaPointApi rutaPointApi = RutaPointApi();
+  @override
+  // final LatLng _ubicacion;
+
+  // MapSearchDelegate();
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [
@@ -32,27 +42,20 @@ class MapSearchDelegate extends SearchDelegate<SearchResult> {
 
   @override
   Widget buildResults(BuildContext context) {
-    return Container(
-      child: ListView(
-        children: [
-          ListTile(
-            leading: const Icon(Icons.person_pin_circle),
-            title: const Text('Add this location'),
-            onTap: () {},
-          )
-        ],
-      ),
-    );
+    final ubicacion = context.read<MiUbicacionBloc>().state.ubicacion;
+
+    return _construirResultadosSugerencias(ubicacion);
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return Container(
-      child: ListView(
+    final ubicacion = context.read<MiUbicacionBloc>().state.ubicacion;
+    if (query.isEmpty) {
+      return ListView(
         children: [
           ListTile(
             leading: const Icon(Icons.person_pin_circle),
-            title: const Text('buildSuggestions'),
+            title: const Text('Historial'),
             onTap: () {
               final searchResult = SearchResult(cancelado: false, manual: true);
               /*  */
@@ -61,7 +64,37 @@ class MapSearchDelegate extends SearchDelegate<SearchResult> {
             },
           )
         ],
-      ),
+      );
+    }
+    return _construirResultadosSugerencias(ubicacion);
+  }
+
+/* listar resultados con un listView */
+  StreamBuilder<SearchGeocoding> _construirResultadosSugerencias(
+      LatLng? ubicacion) {
+    // final futureUbicaciones =
+    // rutaPointApi.getDirections(ubicacion!, query.trim());
+    rutaPointApi.getSugerenciasPorQuery(ubicacion!, query.trim());
+    final sugerenciasStrem = rutaPointApi.stream;
+    return StreamBuilder(
+      stream: sugerenciasStrem,
+      builder: (_, AsyncSnapshot<SearchGeocoding> snapshot) {
+        if (snapshot.hasData) {
+          final lugares = snapshot.data!.features;
+          return ListView.separated(
+            itemBuilder: (_, i) {
+              final lugar = lugares[i];
+              return ListTile(
+                leading: Text(lugar.placeName),
+                onTap: () {},
+              );
+            },
+            separatorBuilder: (_, i) => const Divider(),
+            itemCount: lugares.length,
+          );
+        }
+        return const Center(child: CircularProgressIndicator());
+      },
     );
   }
 }
