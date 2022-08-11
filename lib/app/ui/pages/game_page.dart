e4faee631c91264a8a9db80edbc0877547ff2baf
daniel adame <skaad102@gmail.com>
2022-08-11 09:07:07 -0500

    teclado

--- lib/app/ui/pages/game_page.dart
+++ lib/app/ui/pages/game_page.dart
@@ -1,20 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:rutas_app/app/models/game_model.dart';
 import 'package:rutas_app/app/ui/widget/key_board_widget.dart';
 
-class GamePage extends StatelessWidget {
+class GamePage extends StatefulWidget {
   const GamePage({Key? key}) : super(key: key);
+
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  final gameModel = WordleGameModel();
+
+  @override
+  void initState() {
+    super.initState();
+    WordleGameModel.initGame();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Wordle'),
-            const SizedBox(height: 20),
-            KeyBoard()
-          ],
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('Wordle'),
+              const SizedBox(height: 20),
+              KeyBoard(gameModel)
+            ],
+          ),
         ),
       ),
     );
